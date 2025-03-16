df1eb6536e4750ebceb2720c8f2afa72fefb8fa2
ashik190141 <ashanmahmudashik@gmail.com>
2025-03-16 10:16:10 +0600

    file add

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import Providers from "@/lib/Providers";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Algorithm Visualizer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Providers>
+      <html lang="en">
+        <body className={roboto.className}>{children}</body>
+      </html>
+    </Providers>
+  );
+}
